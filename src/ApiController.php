0b0668f18038c88b401b5afcf619a0398af169a2
César Augusto <cesinhagutierres@gmail.com>
2019-05-01 20:09:18 -0300

    - Criação da controller. - Tipando retorno de metodos.

---
+++ src/ApiController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Preetender\QueryString;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var Model|string
+     */
+    protected $model;
+
+    /**
+     * Iniciar uma nova instancia
+     *
+     * @var bool
+     */
+    protected $modelNewInstance = false;
+
+    /**
+     * @var string
+     */
+    protected $resource;
+
+    /**
+     * Listagem de registros
+     *
+     * @param Interceptor $intercptor
+     * @return mixed
+     */
+    public function index(Interceptor $interceptor)
+    {
+        if (method_exists($this, 'beforeIndex')) {
+            //
+            // Solicitar antes do carregamento
+            //
+            $this->beforeIndex($interceptor->getRequest());
+        }
+
+        abort_if(!$this->checkAttributeExists('model'), 500, 'Model não localizada.');
+
+        $resource = $interceptor->watch(
+            $this->model,
+            $this->modelNewInstance
+        );
+
+        if ($this->checkAttributeExists('resource')) {
+            return $this->resource::collection($resource);
+        }
+
+        return $resource;
+    }
+    /**
+     * Visualizar registro.
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function show(Request $request, $id)
+    {
+        if (method_exists($this, 'beforeShow')) {
+            //
+            // Solicitar antes do carregamento
+            //
+            $this->beforeShow($request);
+        }
+
+        abort_if(!$this->checkAttributeExists('model'), 500, 'Model não localizada.');
+
+        $result = $this->model::findOrFail($id);
+
+        if ($this->checkAttributeExists('resource')) {
+            return new $this->resource($result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Verificar se existe recurso para customizar coleção de dados.
+     *
+     * @param string $attribute
+     * @return bool
+     */
+    private function checkAttributeExists(string $attribute): bool
+    {
+        return array_key_exists($attribute, get_object_vars($this)) && $this->{$attribute} !== null;
+    }
+}
